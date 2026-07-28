46283eeb0c52ae02954f16f2aa87ea81e13d3a3b
ZhuRan1016 <yy1016jp@gmail.com>
2026-07-28 17:03:48 +0900

    Add files via upload

--- README.txt
+++ README.txt
@@ -1,9 +1,10 @@
-SYLDB Web V3.1 悬浮卡片版
-- 点开卡片后原地悬浮显示
-- 鼠标/手指移动时3D倾斜
-- 高光、阴影、回弹
-- 不再跳转外部原图页面
-- 库存管理保留
+SYLDB Web V3.2 关闭按钮修正版
+
+修复：
+- 大图右上角叉号可正常点击
+- 提高叉号层级，避免被3D卡片覆盖
+- 手机触摸与鼠标点击都支持
+- 点击遮罩仍可关闭
 
 上传全部外层文件后打开：
-https://zhuran1016.github.io/sylveon-collection/?v=31
+https://zhuran1016.github.io/sylveon-collection/?v=32
---
+++ service-worker-v32.js
@@ -0,0 +1,5 @@
+const CACHE='syldb-v32-closefix-3201';
+const CORE=['./','index.html','style-v32.css?v=3201','app-v32.js?v=3201','master-v32.json?v=3201','manifest.json'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const cp=r.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return r}).catch(()=>caches.match(e.request)))});
